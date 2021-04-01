65ceae1f8d142afdd4f4e218c67a46e1ed4de38d
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-01 15:18:39 +0200

    Initial Commit

---
+++ HWS-08-Moonshot/HWS_08_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_08_MoonshotApp.swift
+//  HWS-08-Moonshot
+//
+//  Created by Manuel Casocavallo on 01/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_08_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
