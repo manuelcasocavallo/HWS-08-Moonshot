24a60b2f8ccb37d140402ff4f43bba4090555505
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-02 17:35:02 +0200

    Final version

---
+++ HWS-08-Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  HWS-08-Moonshot
+//
+//  Created by Manuel Casocavallo on 01/04/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ HWS-08-Moonshot/Missions.swift
@@ -0,0 +1,41 @@
+//
+//  Missions.swift
+//  HWS-08-Moonshot
+//
+//  Created by Manuel Casocavallo on 01/04/21.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+   
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+
+}
+
